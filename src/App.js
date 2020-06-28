d8d615e501d2d1a55fc932637f98880923358009
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-06-27 21:21:24 -0400

    Added index section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import data from './data/data.json';
+
 export default class App extends Component {
   render() {
+
+    const lists = data.sort();
+
     return (
-      <div>
-        Hello World
-      </div>
+      <section className="container-fluid">
+        <h1>JavaScript</h1>
+        <ul className="list-unstyled">
+          {lists.map(item => (
+            <li key={lists.indexOf(item)}>
+              <small>{item}</small>
+            </li>
+          ))}
+        </ul>
+      </section>
     );
   }
 }
